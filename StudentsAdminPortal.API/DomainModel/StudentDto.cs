d36e42a66a8cce61f3559a338e4791f5510c0738
Swati Darak <Swati.Darak@insperity.com>
2024-03-05 14:54:13 -0600

    Revert "Removed Dto unused models"
    This reverts commit 777b7257be0b936eb32f37bdb8f94998752fa1aa.

---
+++ StudentsAdminPortal.API/DomainModel/StudentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentsAdminPortal.API.DomainModels
+{
+    public class StudentDto
+    {   
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime LastDate { get; set; }
+        public int DepartmentId { get; set; }
+
+
+
+        public StudentDto()
+        {
+            FirstName = "";
+        }
+    }
+}
